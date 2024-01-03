75449d9a8162ef28a3e13737ff56056f04f5a8ee
Semeton <balogunsemeton@gmail.com>
2024-01-03 22:16:26 +0100

    Make test more explicit

--- test/tests.php
+++ test/tests.php
@@ -16,17 +16,21 @@ final class Tests extends TestCase
 
     public function testEncrypt(): void
     {
+        echo "Running testEncrypt...\n";
         $message = (string) "Hello, world!";
         $secretCode = (string) "secret";
 
         $encrypted = $this->cryptoService->encrypt($message, $secretCode);
 
         $this->assertIsString($encrypted);
+        echo "Asserts that the encrypted message is a string.\n";
         $this->assertNotEquals($message, $encrypted);
+        echo "Asserts that plain message is not equal to the encrypted message\n";
     }
 
     public function testDecrypt(): void
     {
+        echo "Running testDecrypt...\n";
         $message = "Hello, world!";
         $secretCode = "secret";
 
@@ -36,10 +40,12 @@ final class Tests extends TestCase
         $decrypted = $cryptoService->decrypt((string)$encrypted, (string)$secretCode);
 
         $this->assertEquals($message, $decrypted);
+        echo "Asserts that plain message is equal to the decrypted message\n";
     }
 
     public function testDecryptWithWrongSecretCode(): void
     {
+        echo "Running testDecryptWithWrongSecretCode...\n";
         $message = (string) "Hello, world!";
         $secretCode = (string) "secret";
         $wrongSecretCode = (string) 'wrongSecret';
@@ -48,5 +54,6 @@ final class Tests extends TestCase
         $decrypted = $this->cryptoService->decrypt($encrypted, $wrongSecretCode);
 
         $this->assertStringContainsString('Error:', $decrypted);
+        echo "Asserts that string contains an error\n";
     }
 }
